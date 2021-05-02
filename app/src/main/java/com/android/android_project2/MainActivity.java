a79b139ac729dd7b64964cd6d7fb155e93cb8187
ekgml4573 <67455705+ekgml4573@users.noreply.github.com>
2021-05-02 14:27:50 +0900

    MainActivity

--- app/src/main/java/com/android/android_project2/MainActivity.java
+++ app/src/main/java/com/android/android_project2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.android.android_project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new MonthViewFragment());
+        fragmentTransaction.commit();
+    }
+    //액션 및 오버플로우 메뉴 추가
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //앱바 월,주
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.month_view:
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, new MonthViewFragment());
+                fragmentTransaction.commit();
+
+                Toast.makeText(getApplicationContext(), "월", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.week_view:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, new WeekViewFragment());
+                fragmentTransaction.commit();
+
+                Toast.makeText(getApplicationContext(), "주", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
